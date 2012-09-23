69fe28dbac42171fba14c873cea7c4240452c803
Eliana Feasley <e.feasley@gmail.com>
2012-09-23 00:17:35 -0500

    Initial commit of the first in what will surely be a long string of tiny twitter clones. tweet, tweet, tweet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'e5e218f8beba1d028df8e61d90d5f3de5802a8810421feada3651b56cdbc36cae660867b048224f245be7c33ea401f2aa031d0e71a03ce0cde78f607e7fefac3'
